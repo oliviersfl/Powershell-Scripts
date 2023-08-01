1f91fa02067b25c361e1bf5266e045d7e7ba7787
Olivier Sin Fai Lam <oliviersfl@gmail.com>
2023-08-01 12:11:02 +0400

    Add logic to hard delete branches

--- BranchCleanup.ps1
+++ BranchCleanup.ps1
@@ -1,32 +1,22 @@
 <#
 .SYNOPSIS
-    BranchCleanup.ps1 is a PowerShell script that identifies and deletes local Git branches that do not have a corresponding remote branch and are not currently checked out in any worktree.
+BranchCleanup.ps1 is a script to clean up local git branches that are no longer existing in the remote repository.
+
+.DESCRIPTION
+The script lists all local branches, checks each one if it exists in the remote repository, and deletes it if not. 
+Before each deletion, the script asks for user confirmation. 
+If the branch is not fully merged and cannot be safely deleted, the script asks for user confirmation before force deletion.
 
 .PARAMETERS
-    -NoFetch: A switch parameter. If specified, the script will skip the 'git fetch' command. Useful when you've recently fetched remote updates and want to avoid fetching again.
-    
-.USAGE
-    ./BranchCleanup.ps1               # Run the script normally.
-    ./BranchCleanup.ps1 -NoFetch      # Run the script without fetching updates from remote.
-    
-.DESCRIPTION
-    The script first checks if the NoFetch parameter is not specified, in which case it fetches updates from the remote repository. It then identifies all local and remote branches.
+-NoFetch: A switch parameter to control whether the script fetches the latest data from the remote repository. 
+If the switch is specified, the script won't fetch from the remote repository.
 
-    The script traverses through each local worktree, identifies the currently checked-out branch, and excludes these branches from the list of local branches.
-
-    It then checks the modified list of local branches against the list of remote branches, identifying which local branches do not have a corresponding remote branch. These branches are added to the deletion list.
-
-    If any branches are found to be on the deletion list, the script prompts the user for confirmation to proceed with deletion. Upon user confirmation, it attempts to delete each branch, displaying a success or failure message for each one.
-
-    If no branches are on the deletion list, the script informs the user that there are no branches to delete.
-
-    Finally, the script returns the working directory to the original location.
+.EXAMPLE
+PS C:\> .\BranchCleanup.ps1 -NoFetch
+In this example, the script will skip fetching from the remote repository.
 
 .NOTES
-    The script handles errors resulting from the 'git branch -d' command, hence, it will not stop execution if a branch cannot be deleted.
-    If the script fails to delete a branch, it will print an error message and continue with the next branch.
-
-    It's important to note that the script does not handle other potential Git errors, so if other Git commands fail, they could affect the script's behavior.
+The script assumes that your current location is the root of your Git repository. If not, you may need to navigate there first or adapt the script.
 #>
 
 param(
@@ -62,8 +52,9 @@ Set-Location -Path $original_location
 # Filter out checked-out branches
 $local_branches = $local_branches | Where-Object { $_ -notin $worktree_branches }
 
-# Initialize deletion array
+# Initialize deletion and hard deletion arrays
 $branches_to_delete = @()
+$branches_to_force_delete = @()
 
 foreach ($branch in $local_branches) {
     if ($branch -notin $remote_branches) {
@@ -77,26 +68,62 @@ if ($branches_to_delete.Length -gt 0) {
     $branches_to_delete | %{ echo $_ }
 
     # Ask for user confirmation
-    Write-Host "Do you want to proceed with deletion? [Y/n]" -NoNewline
+    Write-Host "Do you want to proceed with deletion? [Y/n]" -NoNewline -ForegroundColor Cyan
+    $user_input = [System.Console]::ReadKey($true).Key
+	Write-Host ""
+
+    if ($user_input -eq 'Y') {
+        echo "" # Just to add a newline after the prompt
+
+        foreach ($branch in $branches_to_delete) {
+            # Run the git command as a separate process
+			$process = Start-Process -FilePath "git" -ArgumentList "branch -d $branch" -NoNewWindow -Wait -PassThru
+			$exitCode = $process.ExitCode
+
+			if ($exitCode -ne 0) {
+				Write-Host "Branch $branch is not fully merged.`n" -ForegroundColor Red
+				$branches_to_force_delete += $branch
+			} else {
+				Write-Host "Branch $branch deleted.`n"
+			}
+        }
+		
+		Write-Host ""
+    } else {
+        echo "" # Just to add a newline after the prompt
+        Write-Host "Branch deletion aborted by user." -ForegroundColor Yellow
+        return
+    }
+}
+
+# If there are branches to be force deleted, prompt user for confirmation
+if ($branches_to_force_delete.Length -gt 0) {
+    echo "The following branches could not be deleted safely and may require a hard delete:"
+    $branches_to_force_delete | %{ echo $_ }
+
+    Write-Host "Do you want to proceed with hard deletion? This can cause loss of commits. [Y/n]" -NoNewline -ForegroundColor Cyan
     $user_input = [System.Console]::ReadKey($true).Key
 
     if ($user_input -eq 'Y') {
         echo "" # Just to add a newline after the prompt
-        foreach ($branch in $branches_to_delete) {
-            git branch -d $branch
+
+        # Force delete branches
+        foreach ($branch in $branches_to_force_delete) {
+            git branch -D $branch
             if ($LASTEXITCODE -eq 0) {
-                echo "Branch $branch deleted."
-                echo ""
+                Write-Host "Branch $branch forcefully deleted." -ForegroundColor Yellow
             } else {
-                echo "Failed to delete branch $branch."
+                Write-Host "Failed to forcefully delete branch $branch." -ForegroundColor Red
             }
         }
     } else {
-        echo "" # Just to add a newline after the prompt
-        echo "Branch deletion aborted by user."
+        Write-Host "" # Just to add a newline after the prompt
+        Write-Host "Forceful branch deletion aborted by user." -ForegroundColor Yellow
     }
-} else {
-    echo "No branches to delete."
+}
+
+if ($branches_to_delete.Length -eq 0 -and $branches_to_force_delete.Length -eq 0) {
+    Write-Host "No branches to delete." -ForegroundColor Yellow
 }
 
 # Return to the original location at the end of the script
