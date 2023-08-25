e9a21f363b94b5bebd91c24471cacb6d169a4b87
Olivier <olivier.sinfailam@ceridian.com>
2023-08-25 10:12:40 +0400

    Update PingTest Script to fix cursor issues

--- PingTest.ps1
+++ PingTest.ps1
@@ -2,53 +2,65 @@
 $targetIP = "8.8.8.8"
 $pingTimes = New-Object System.Collections.Queue
 $timeoutCount = 0
-$firstIteration = $true
 
-while ($true) {
-    $currentTime = (Get-Date).ToString('hh:mm:ss')
-    $pingOutput = ping $targetIP -n 1
-    $result = $pingOutput | Select-String "Reply from"
-    $timeout = $pingOutput | Select-String "Request timed out."
+try {
+    while ($true) {
+		$currentTime = (Get-Date).ToString('hh:mm:ss')
+		$pingOutput = ping $targetIP -n 1
+		$result = $pingOutput | Select-String "Reply from"
+		$timeout = $pingOutput | Select-String "Request timed out."
 
-    if ($result) {
-        if ($pingTimes.Count -ge 100) {
-            $oldestPing = $pingTimes.Dequeue()
-            if ($oldestPing -eq "timeout") {
-                $timeoutCount--
-            }
-        }
-        # Extract ping time in ms
-        $time = [int](($result -split "time=")[1] -split "ms")[0].Trim()
-        $pingTimes.Enqueue($time)
-    } elseif ($timeout) {
-        if ($pingTimes.Count -ge 100) {
-            $oldestPing = $pingTimes.Dequeue()
-            if ($oldestPing -ne "timeout") {
-                $timeoutCount--
-            }
-        }
-        $pingTimes.Enqueue("timeout")
-        $timeoutCount++
-    }
+		if ($result) {
+			if ($pingTimes.Count -ge 100) {
+				$oldestPing = $pingTimes.Dequeue()
+				if ($oldestPing -eq "timeout") {
+					$timeoutCount--
+				}
+			}
+			# Extract ping time in ms
+			$time = [int](($result -split "time=")[1] -split "ms")[0].Trim()
+			$pingTimes.Enqueue($time)
+		} elseif ($timeout) {
+			if ($pingTimes.Count -ge 100) {
+				$oldestPing = $pingTimes.Dequeue()
+				if ($oldestPing -ne "timeout") {
+					$timeoutCount--
+				}
+			}
+			$pingTimes.Enqueue("timeout")
+			$timeoutCount++
+		}
 
-    # Calculating stats for the last 100 pings
-    $validPingTimes = $pingTimes | Where-Object { $_ -ne "timeout" }
-    $averagePing = ($validPingTimes | Measure-Object -Average).Average
-    $minPing = ($validPingTimes | Measure-Object -Minimum).Minimum
-    $maxPing = ($validPingTimes | Measure-Object -Maximum).Maximum
+		# Calculating stats for the last 100 pings
+		$validPingTimes = $pingTimes | Where-Object { $_ -ne "timeout" }
+		$averagePing = ($validPingTimes | Measure-Object -Average).Average
+		$minPing = ($validPingTimes | Measure-Object -Minimum).Minimum
+		$maxPing = ($validPingTimes | Measure-Object -Maximum).Maximum
 
-    # For subsequent iterations, move cursor up two lines to overwrite
-    if (-not $firstIteration) {
-        $pos = $host.UI.RawUI.CursorPosition
-        $pos.Y -= 2
-        $host.UI.RawUI.CursorPosition = $pos
-    } else {
-        $firstIteration = $false
-    }
+		# Clear current line and write the stats
+		Write-Host "`r" -NoNewline
+		Write-Host (" " * ($host.UI.RawUI.WindowSize.Width - 1)) -NoNewline
+		Write-Host "`r" -NoNewline
+		Write-Host "`r[$currentTime] Avg: $($averagePing -as [int])ms | Min: $($minPing)ms | Max: $($maxPing)ms | Timeouts (last 100): $timeoutCount"
 
-    # Write average, min, max, timeout count, then write the result
-    Write-Host "[$currentTime] Avg: $($averagePing -as [int])ms | Min: $($minPing)ms | Max: $($maxPing)ms | Timeouts (last 100): $timeoutCount"
-    Write-Host "$result$timeout"
+		# Clear next line and write the result
+		Write-Host "`r" -NoNewline
+		Write-Host (" " * ($host.UI.RawUI.WindowSize.Width - 1)) -NoNewline
+		Write-Host "`r" -NoNewline
+		Write-Host "`r$result$timeout"
 
-    Start-Sleep -Milliseconds 1000 # Optional: Sleep for 1 second
+		# After writing, move the cursor up two lines to be in position for the next iteration
+		$pos = $host.UI.RawUI.CursorPosition
+		$pos.Y -= 2
+		$host.UI.RawUI.CursorPosition = $pos
+
+		Start-Sleep -Milliseconds 1000 # Optional: Sleep for 1 second
+	}
+}
+
+finally {
+    # Reset cursor position to where the latest output is:
+    $pos = $host.UI.RawUI.CursorPosition
+    $pos.Y += 2
+    $host.UI.RawUI.CursorPosition = $pos
 }
