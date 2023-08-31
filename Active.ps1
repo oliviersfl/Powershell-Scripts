b9a80d1b1072e9b5efb113cbbf2b7ea15e368480
Olivier <olivier.sinfailam@ceridian.com>
2023-08-31 17:16:11 +0400

    Add Active.ps1

---
+++ Active.ps1
@@ -0,0 +1,70 @@
+# Define a P/Invoke to call a native function for getting last input time
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+
+public class UserInput {
+    [DllImport("user32.dll", SetLastError=false)]
+    private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct LASTINPUTINFO {
+        public uint cbSize;
+        public uint dwTime;
+    }
+
+    public static uint GetLastInputTime() {
+        LASTINPUTINFO info = new LASTINPUTINFO();
+        info.cbSize = (uint)Marshal.SizeOf(info);
+        GetLastInputInfo(ref info);
+        return info.dwTime;
+    }
+}
+"@
+
+# Initialize variables
+$idleTime = 0.05 # idle time in minutes; you can change this value or set it through a parameter
+$lastActiveTime = $null  # Initialize to $null
+$activeDuration = [TimeSpan]::FromSeconds(0)
+$totalActiveDuration = [TimeSpan]::FromSeconds(0)
+$isActive = $false  # New flag variable to track active state
+
+# Loop indefinitely
+while ($true) {
+    Start-Sleep -Seconds 1
+
+    $lastBootUpTime = (Get-CimInstance Win32_OperatingSystem).LastBootUpTime
+
+    # Get system uptime and last input time
+    $systemUptime = [math]::Round(((Get-Date) - $lastBootUpTime).TotalMilliseconds)
+    $lastInputTime = [UserInput]::GetLastInputTime()
+
+    # Calculate idle time
+    $idleDuration = ($systemUptime - $lastInputTime) / 1000
+
+    $consoleWidth = $Host.UI.RawUI.WindowSize.Width  # Get the console window width
+    $maxOutputLength = $consoleWidth - 1  # Adjust the max output length to fit within the console window
+
+    if ($idleDuration -lt ($idleTime * 60)) {
+        $activeDuration = $activeDuration.Add([TimeSpan]::FromSeconds(1))
+        $totalActiveDuration = $totalActiveDuration.Add([TimeSpan]::FromSeconds(1))
+        
+        # Set $lastActiveTime only when first becoming active
+        if (-not $isActive) {
+            $lastActiveTime = Get-Date
+            $isActive = $true
+        }
+
+        $output = "You've been active for $($activeDuration.ToString('hh\:mm\:ss')) since $lastActiveTime. Total active time in this session: $($totalActiveDuration.ToString('hh\:mm\:ss'))"
+    } else {
+        $activeDuration = [TimeSpan]::FromSeconds(0)
+        $isActive = $false  # Reset flag when becoming idle
+        $output = "Idle time exceeded $idleTime minutes. Resetting active time counter. Total active time in this session so far: $($totalActiveDuration.ToString('hh\:mm\:ss'))"
+    }
+
+    # Pad the output string with spaces and then trim it to fit within the console window
+    $paddedOutput = $output.PadRight($maxOutputLength).Substring(0, $maxOutputLength)
+
+    Write-Host "`r$paddedOutput" -NoNewline
+}
